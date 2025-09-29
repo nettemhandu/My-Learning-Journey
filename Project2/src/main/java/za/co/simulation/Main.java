f638e95262cf0eb5a2ae79cf1c987242e59f4767
nettemhandu <antonetteliebe@icloud.com>
2025-09-29 20:59:19 +0200

    creating the class

---
+++ Project2/src/main/java/za/co/simulation/Main.java
@@ -0,0 +1,18 @@
+package za.co.simulation;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+
+    public void validate() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your Password: ");
+        String input = scanner.nextLine();
+
+        for ()
+    }
+}
