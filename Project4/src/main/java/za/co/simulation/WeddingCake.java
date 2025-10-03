27b8af6595634597a10478a356cc4d43335a6e95
nettemhandu <antonetteliebe@icloud.com>
2025-10-03 19:49:33 +0200

    added wedding cake class

---
+++ Project4/src/main/java/za/co/simulation/WeddingCake.java
@@ -0,0 +1,13 @@
+package za.co.simulation;
+
+public class WeddingCake extends Cake {
+    private int tiers;
+
+    public WeddingCake(String flavor) {
+        super(flavor);
+        flavor = "Vanilla";
+    }
+
+
+
+}
