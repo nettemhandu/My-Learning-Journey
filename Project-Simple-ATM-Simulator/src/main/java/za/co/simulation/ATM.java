f25909de7c12b240e87d0f0f43c15015a42ff8af
nettemhandu <antonetteliebe@icloud.com>
2025-10-06 15:05:59 +0200

    made ATM class for input and validations

---
+++ Project-Simple-ATM-Simulator/src/main/java/za/co/simulation/ATM.java
@@ -0,0 +1,11 @@
+package za.co.simulation;
+
+import java.util.Scanner;
+
+public class ATM {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to your Bank Account");
+    }
+}
