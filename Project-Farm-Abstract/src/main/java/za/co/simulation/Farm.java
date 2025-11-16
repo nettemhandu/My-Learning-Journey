0bcf32ef25f1dbee0b03bab4e8161b04eb448378
nettemhandu <antonetteliebe@icloud.com>
2025-11-16 19:48:20 +0200

    added main function to test implementation

---
+++ Project-Farm-Abstract/src/main/java/za/co/simulation/Farm.java
@@ -0,0 +1,9 @@
+package za.co.simulation;
+
+public class Farm {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the farm!");
+        Pig piggy = new Pig();
+        piggy.makeSound();
+    }
+}
