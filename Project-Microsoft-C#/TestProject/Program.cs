457e3b7001541760e36c49cb2bae431d21f37708
nettemhandu <antonetteliebe@icloud.com>
2025-12-28 21:40:44 +0200

    added git ignore file

--- Project-Microsoft-C#/TestProject/Program.cs
+++ Project-Microsoft-C#/TestProject/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// using System;
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
