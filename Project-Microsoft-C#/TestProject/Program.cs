901ac4464a371747f30eed829e534103e62bbb9a
nettemhandu <antonetteliebe@icloud.com>
2025-12-29 20:57:54 +0200

    new exercise

--- Project-Microsoft-C#/TestProject/Program.cs
+++ Project-Microsoft-C#/TestProject/Program.cs
@@ -1,31 +1,9 @@
-﻿Random dice = new Random();
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+// Your code goes here
 
-int total = roll1 + roll2 + roll3;
-
-if (total >= 15)
-{
-    Console.WriteLine("You win!");
+if (daysUntilExpiration <= 10){
+    Console.WriteLine("");
 }
-else
-{
-    Console.WriteLine("Sorry, you lose.");
-}
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-{
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples! +6 bonus to total!");
-        total += 6;
-    } else
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
-}
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
