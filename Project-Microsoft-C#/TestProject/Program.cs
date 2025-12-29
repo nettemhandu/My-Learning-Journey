f69c45bc312dfbbcba58e17afc89366703283c8c
nettemhandu <antonetteliebe@icloud.com>
2025-12-29 21:10:10 +0200

    refactored solution

--- Project-Microsoft-C#/TestProject/Program.cs
+++ Project-Microsoft-C#/TestProject/Program.cs
@@ -2,24 +2,26 @@
 int daysUntilExpiration = random.Next(12);
 int discountPercentage = 0;
 
-// Your code goes here
-
-if (daysUntilExpiration <= 10){
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-else if(daysUntilExpiration <= 5)
-{
-    Console.WriteLine($"Your subscription expires {daysUntilExpiration} in days. Renew now and save 10%!");
-}
-else if(daysUntilExpiration <= 1)
-{
-    Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
-}
-else if(daysUntilExpiration <= 0)
+if (daysUntilExpiration == 0)
 {
     Console.WriteLine("Your subscription has expired.");
 }
-else if(daysUntilExpiration > 10)
+else if (daysUntilExpiration == 1)
 {
-    Console.Write(" ");
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
 }
