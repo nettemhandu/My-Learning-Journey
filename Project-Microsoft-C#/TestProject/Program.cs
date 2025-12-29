212f95851fb913d60a4928ad8a08d50efd09b137
nettemhandu <antonetteliebe@icloud.com>
2025-12-29 21:02:57 +0200

    debugging nested statements

--- Project-Microsoft-C#/TestProject/Program.cs
+++ Project-Microsoft-C#/TestProject/Program.cs
@@ -5,5 +5,13 @@ int discountPercentage = 0;
 // Your code goes here
 
 if (daysUntilExpiration <= 10){
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else if(daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires {daysUntilExpiration} in days. Renew now and save 10%!");
+}
+else if(daysUntilExpiration <= 1)
+{
     Console.WriteLine("");
 }
