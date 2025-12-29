cee45aaf8e1e8b3d526411499c0e24ad7342e72b
nettemhandu <antonetteliebe@icloud.com>
2025-12-29 20:35:12 +0200

    adding if statements

--- Project-Microsoft-C#/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ Project-Microsoft-C#/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+457e3b7001541760e36c49cb2bae431d21f37708")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f170142fec9ceab30a97b54f444bf43a591d630")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
