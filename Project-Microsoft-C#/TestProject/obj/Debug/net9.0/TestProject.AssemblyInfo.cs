8767e8a766b6692f97302e696eb610a55ecbec4a
nettemhandu <antonetteliebe@icloud.com>
2026-01-01 09:48:43 +0200

    reassigning array index

--- Project-Microsoft-C#/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ Project-Microsoft-C#/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe86034b941ee6f6ee320537f2cf1d4ed955b38f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2288d6d211cbe81c5ea4f4d6b4f4eedabcd63aca")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
