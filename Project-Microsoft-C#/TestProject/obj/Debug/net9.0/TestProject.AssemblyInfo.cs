fe86034b941ee6f6ee320537f2cf1d4ed955b38f
nettemhandu <antonetteliebe@icloud.com>
2026-01-01 09:29:23 +0200

    printing array items

--- Project-Microsoft-C#/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ Project-Microsoft-C#/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f9de55f47373135e5192003be62493c1420daf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a72e97603e1b652161c59f20187193c96027793")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
