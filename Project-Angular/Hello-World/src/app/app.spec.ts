95dc77f0b377b33ee7fb97be7eca719e9ed8aceb
nettemhandu <antonetteliebe@icloud.com>
2026-01-06 11:44:27 +0200

    started angular app

--- Project-Angular/Hello-World/src/app/app.spec.ts
+++ Project-Angular/Hello-World/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Hello-World');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hello-world');
   });
 });
