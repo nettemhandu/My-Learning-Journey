8ee59f387aecb45cb344e7b41fb00f649063d224
nettemhandu <antonetteliebe@icloud.com>
2026-01-07 16:13:15 +0200

    adding component

--- Project-Angular/Hello-World/src/app/emp-info/emp-info.ts
+++ Project-Angular/Hello-World/src/app/emp-info/emp-info.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { RecordsService } from "../records"
 
 @Component({
   selector: 'app-emp-info',
   imports: [],
   templateUrl: './emp-info.html',
   styleUrl: './emp-info.css',
+  providers: [RecordsService]
 })
 
 export class EmpInfo implements OnInit {
