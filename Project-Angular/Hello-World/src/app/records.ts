90bdba621a52b09967beea967763de154c2b6b49
nettemhandu <antonetteliebe@icloud.com>
2026-01-07 14:51:47 +0200

    created two arrays

--- Project-Angular/Hello-World/src/app/records.ts
+++ Project-Angular/Hello-World/src/app/records.ts
@@ -4,5 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Records {
-  
+  info1: String[] = ['Nette', 'E234', 'ne@abc.net'];
+  info1: String[] = ['Anto', 'E235', 'an@abc.net'];
+  info1: String[] = ['Pana', 'E236', 'pa@abc.net'];
 }
