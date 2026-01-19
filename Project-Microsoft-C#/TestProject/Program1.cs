8158458e25f00261f30fd0f87a09be0847d9303f
nettemhandu <antonetteliebe@icloud.com>
2026-01-19 13:09:31 +0200

    arrays in c#

--- Project-Microsoft-C#/TestProject/Program1.cs
+++ Project-Microsoft-C#/TestProject/Program1.cs
@@ -1,31 +1,31 @@
-/*
-   This code reverses a message, counts the number of times 
-   a particular character appears, then prints the results
-   to the console window.
- */
+// /*
+//    This code reverses a message, counts the number of times 
+//    a particular character appears, then prints the results
+//    to the console window.
+//  */
 
-string str = "The quick brown fox jumps over the lazy dog.";
+// string str = "The quick brown fox jumps over the lazy dog.";
 
-// convert the message into a char array
-char[] originalMessage = str.ToCharArray();
+// // convert the message into a char array
+// char[] originalMessage = str.ToCharArray();
 
-// Reverse the chars
-Array.Reverse(message);
+// // Reverse the chars
+// Array.Reverse(message);
 
-int letterCount = 0;
+// int letterCount = 0;
 
-// count the o's
-foreach (char letter in message)
-    {
-    if (i == 'o')
-    {
-        letterCount++;
-    }
-    }
+// // count the o's
+// foreach (char letter in message)
+//     {
+//     if (i == 'o')
+//     {
+//         letterCount++;
+//     }
+//     }
 
-// convert it back to a string
-string newMessage = new String(message);
+// // convert it back to a string
+// string newMessage = new String(message);
 
-// print it out
-Console.WriteLine(newMessage);
-Console.WriteLine($"'o' appears {x} times.");
+// // print it out
+// Console.WriteLine(newMessage);
+// Console.WriteLine($"'o' appears {x} times.");
