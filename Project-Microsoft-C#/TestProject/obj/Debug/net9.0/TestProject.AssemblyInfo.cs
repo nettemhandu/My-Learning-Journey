e7c93f8f35c88801d9e88bd47d7bcceb0c2a4a5b
nettemhandu <antonetteliebe@icloud.com>
2026-01-28 13:01:01 +0200

    added exercism folder

--- Project-Microsoft-C#/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ Project-Microsoft-C#/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+091a234f24c11723703203f23a6e41cadcb45d99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c81671b9850e36ab717607697d9c32c8dbaa71b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
