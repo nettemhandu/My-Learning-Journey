caea712f7d3feb1acab25f343eef712daf6e16a9
nettemhandu <antonetteliebe@icloud.com>
2026-02-12 12:38:19 +0200

    added nagarro

--- Project-Microsoft-C#/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ Project-Microsoft-C#/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c81671b9850e36ab717607697d9c32c8dbaa71b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+884fdb97f2db156b1b797b111081b8115b4ff031")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
